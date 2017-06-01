99233bc6b74e60f6699554537e801dd90596ba03
Lance Shao <115008352@qq.com>
2017-06-01 20:05:18 +1000

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+var card = new Vue({
+  el: '#card',
+  data: {
+    title: 'Daily Plan',
+    items: [
+      
+    ]
+  },
+  methods: {
+    addItem: function(){
+      var input = document.getElementById("itemForm");
+
+      if(input.value !== '') {
+        this.items.push({
+          text: input.value
+        })
+        input.value = '';
+      }
+    },
+    deleteItem: function(index){
+      this.items.splice(index,1)
+    }
+  }
+})
